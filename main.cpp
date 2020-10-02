ebd868eb970cc83c3e258cd50d187fd6ffb10742
Wim Valcke <wim.valcke@be.bepco.com>
2020-10-02 09:52:22 +0200

    Better lock screen implementation, portrait mode fixed by manifest

--- main.cpp
+++ main.cpp
@@ -1,9 +1,36 @@
 #include <QtWidgets/QApplication>
 #include <QQmlApplicationEngine>
 #include <QtQml/QQmlContext>
+#ifdef Q_OS_ANDROID
 #include <QtAndroid>
+#include <QtAndroidExtras/QAndroidJniEnvironment>
+#endif
 #include "DataPool.h"
-#include "LockHelper.h"
+
+#ifdef Q_OS_ANDROID
+void keep_screen_on(bool on) {
+  QtAndroid::runOnAndroidThread([on]{
+    QAndroidJniObject activity = QtAndroid::androidActivity();
+    if (activity.isValid()) {
+      QAndroidJniObject window =
+          activity.callObjectMethod("getWindow", "()Landroid/view/Window;");
+
+      if (window.isValid()) {
+        const int FLAG_KEEP_SCREEN_ON = 128;
+        if (on) {
+          window.callMethod<void>("addFlags", "(I)V", FLAG_KEEP_SCREEN_ON);
+        } else {
+          window.callMethod<void>("clearFlags", "(I)V", FLAG_KEEP_SCREEN_ON);
+        }
+      }
+    }
+    QAndroidJniEnvironment env;
+    if (env->ExceptionCheck()) {
+      env->ExceptionClear();
+    }
+  });
+}
+#endif
 
 int main(int argc, char *argv[])
 {
@@ -26,8 +53,9 @@ int main(int argc, char *argv[])
     }, Qt::QueuedConnection);
     engine.load(url);
 
-    QtAndroid::androidActivity().callStaticMethod<void>("org/bep/jni/ScreenLockInterface", "initializeData", "()V");
-    QtAndroid::androidActivity().callMethod<void>("setRequestedOrientation", "(I)V", 1);
+#ifdef Q_OS_ANDROID
+    keep_screen_on(true);
+#endif
 
     return app.exec();
 }
